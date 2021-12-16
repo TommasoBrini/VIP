44ae17ecf1f26652ffc8c0931fa469050c1f7c1b
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-12-16 12:24:22 +0100

    Fixed slider buttons

---
+++ template/products.php
@@ -0,0 +1,1 @@
+<?php echo "<h1>SIAMO IN PRODUCTS</h1>"; ?>
--- template/slider.php
+++ template/slider.php
@@ -1,1 +1,13 @@
-<button class="selected"><?php echo $templateParams["button1"] ?></button><button class="unset"><?php echo $templateParams["button2"] ?></button>
+<button class="<?php 
+    if($templateParams["class1"]){
+        echo "selected";   
+    } else {
+        echo "unset";
+    }
+    ?>" onclick="<?php echo "window.location.href='".$templateParams["href1"]."'"; ?>"><?php echo $templateParams["button1"] ?></button><button class="<?php 
+    if($templateParams["class2"]){
+        echo "selected";   
+    } else {
+        echo "unset";
+    }
+    ?>" onclick="<?php echo "window.location.href='".$templateParams["href2"]."'"; ?>"><?php echo $templateParams["button2"] ?></button>
