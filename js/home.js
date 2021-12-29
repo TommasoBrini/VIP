2a19207fac52a97630fd126ad333cbd74548be9b
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-12-29 20:00:08 +0100

    NON VA NULLA

--- js/home.js
+++ js/home.js
@@ -19,32 +19,48 @@ function selectClick(e) {
     e.stopPropagation();
 }
 
-function timer(year, month, day, time, id){
+function timer(year, month, day, time, id, state){
     var countDownDate = new Date(month + " " + day + ", " + year + " " + time + ":00").getTime();
-
+        if(state == 'FINISHED'){
+            $("button#timer" + id).replaceWith("<button class='timer' id='timer" +  id +"'>00:00:00</button>");
+        } else {
             // Update the count down every 1 second
             var x = setInterval(function() {
 
-            // Get today's date and time
-            var now = new Date().getTime();
-                            
-            // Find the distance between now and the count down date
-            var distance = countDownDate - now;
-
-            // Time calculations for hours, minutes and seconds
-            var hours = Math.floor(distance / (1000 * 60 * 60));
-            var minutes = Math.floor((distance / (1000 * 60 * 60) - hours) * 60);
-            var seconds = Math.floor((((distance / (1000 * 60 * 60) - hours) * 60) - minutes) * 60);
-
-            // Output the result in an element with id="demo"
-            $("button#timer" + id).replaceWith("<button class='timer' id='timer" +  id +"'>" + hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds) + "</button>");
+                // Get today's date and time
+                var now = new Date().getTime();
                                 
-                // If the count down is over, write some text 
-            if (distance < 0) {
-                clearInterval(x);
-                $("button#timer" + id).replaceWith("<button class='timer' id='timer" +  id +"'>00:00:00</button>");
-            }
-        }, 1000);
+                // Find the distance between now and the count down date
+                var distance = countDownDate - now;
+    
+                // Time calculations for hours, minutes and seconds
+                var hours = Math.floor(distance / (1000 * 60 * 60));
+                var minutes = Math.floor((distance / (1000 * 60 * 60) - hours) * 60);
+                var seconds = Math.floor((((distance / (1000 * 60 * 60) - hours) * 60) - minutes) * 60);
+    
+                // Output the result in an element with id="demo"
+                $("button#timer" + id).replaceWith("<button class='timer' id='timer" +  id +"'>" + hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds) + "</button>");
+                /*  
+                    // If the count down is over, write some text 
+                if (distance < 0) {
+                    clearInterval(x);
+                    if(state == 'BEFORE'){
+                        newState = 'STARTED';
+                    } else {
+                        newState = 'AFTER';
+                    }
+                    $.ajax({
+                        url: "js/home.php&f=updateAuctionState?id=" + id + "&state=" + newState,
+                        type: "GET",
+                        success: function(data){
+    
+                        }
+                     });
+    
+                    document.location.reload(true);
+                }*/
+            }, 1000);
+        }
 }
 
 /*
