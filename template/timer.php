f080ed2594536907424310619f069b4515e09b7d
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2021-12-30 17:11:55 +0100

    solved auction's timer and status update

--- template/timer.php
+++ template/timer.php
@@ -1,31 +1,27 @@
 <script type='text/javascript'>
-        if("<?php echo $asta["Stato"] ?>" == "AFTER"){
+
+        var startDate = new Date("<?php echo getMounth($asta["MeseInizio"])." ".$asta["GiornoInizio"].", ".$asta["AnnoInizio"]." ".$asta["OraInizio"].":00" ?>").getTime();
+        
+        var endDate = new Date("<?php echo getMounth($asta["MeseFine"])." ".$asta["GiornoFine"].", ".$asta["AnnoFine"]." ".$asta["OraFine"].":00" ?>").getTime();
+        
+        var now = new Date().getTime();
+        var startDistance = startDate - now;
+        var endDistance = endDate - now;
+
+        if(endDistance <= 0){
             $("button#timer" + <?php echo $asta["IDProdotto"] ?>).replaceWith("<button class='timer' id='timer" +  <?php echo $asta["IDProdotto"] ?> +"'>00:00:00</button>");
-        } else {
-            var countDownDate = null;
-            <?php $nextState = "";?>
-
-            if("<?php echo $asta["Stato"] ?>" == "BEFORE") {
-                countDownDate = new Date("<?php echo getMounth($asta["MeseInizio"])." ".$asta["GiornoInizio"].", ".$asta["AnnoInizio"]." ".$asta["OraInizio"].":00" ?>").getTime();
-            } else if("<?php echo $asta["Stato"] ?>" == "STARTED"){
-                countDownDate = new Date("<?php echo getMounth($asta["MeseFine"])." ".$asta["GiornoFine"].", ".$asta["AnnoFine"]." ".$asta["OraFine"].":00" ?>").getTime();
-            }
-            
-            <?php if($asta["Stato"] == "BEFORE"){
-                    $nextState = "STARTED";
-            } 
-            if($asta["Stato"] == "STARTED") {
-                $nextState = "AFTER";
-            }   ?>
+        } else {            
             // Update the count down every 1 second
-            var x = setInterval(function() {
-
-                // Get today's date and time
-                var now = new Date().getTime();
-                                
+            var x = setInterval(function() {                             
                 // Find the distance between now and the count down date
-                var distance = countDownDate - now;
-    
+
+                now = new Date().getTime();
+                var distance;
+                if(startDistance <=0){
+                    distance = endDate - now;
+                } else {
+                    distance = startDate - now;
+                }
                 // Time calculations for hours, minutes and seconds
                 var hours = Math.floor(distance / (1000 * 60 * 60));
                 var minutes = Math.floor((distance / (1000 * 60 * 60) - hours) * 60);
@@ -34,16 +30,12 @@
                 hours < 10 ? hours = '0'+hours : hours = hours;
                 minutes < 10 ? minutes = '0'+minutes : minutes = minutes;
                 seconds < 10 ? seconds = '0'+seconds : seconds = seconds;
-
                 // Output the result in an element with id="demo"
                 $("<?php echo "button#timer".$asta["IDProdotto"] ?>").replaceWith("<?php echo "<button class='timer' id='timer".$asta["IDProdotto"]."'>" ?>" + hours + ":" + minutes + ":" + seconds + "</button>");  
 
-                // If the count down is over, write some text 
-                if (distance < 0) {
+                // If the count down is over, write some text
+                if (distance <= 0) {
                     clearInterval(x);
-                    <?php
-                        $dbh->updateAuctionState($asta["IDProdotto"], $nextState);
-                    ?>
                     document.location.reload(true);
                 }
             }, 1000);
