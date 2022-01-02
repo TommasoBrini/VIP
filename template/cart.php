8dab06dfba95cf052eb8bb09cc013363b31846d6
Emanuele Orlietti <emanuele.orlietti@studio.unibo.it>
2022-01-02 20:51:09 +0100

    Cart started

---
+++ template/cart.php
@@ -0,0 +1,21 @@
+    <h1>CARRELLO</h1>
+    <table>
+    <tr>
+        <th> </th>
+        <th>Photo</th>
+        <th>Description</th>
+        <th>Unit price</th>
+        <th>Quantity</th>
+        <th>Total</th>
+        <th> </th>
+    </tr>
+    <tr>
+        <td><input type='checkbox' id='select' id='select'></td>
+        <td>Foto</td>
+        <td>Prezzo</td>
+        <td>Prezzo unitario</td>
+        <td><input type='number' id='quantity' name='quantity'></td>
+        <td>Totale</td>
+        <td><img type="button" src="./img/garbage.png" id="garbage" /><br>
+    </tr>
+    </table>
