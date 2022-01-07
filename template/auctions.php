e5bdbc5dd3f6f2501856b311fa43a2d82490c2e0
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2022-01-07 16:14:06 +0100

    don't take old auctions then one week

--- template/auctions.php
+++ template/auctions.php
@@ -1,10 +1,11 @@
 <?php foreach($templateParams["auctions"] as $asta){
     $date = new DateTime();
+    $oneWeekAgo = new DateTime();
+    date_sub($oneWeekAgo, date_interval_create_from_date_string('7 days'));
     $start = $asta["AnnoInizio"]."-".$asta["MeseInizio"]."-".$asta["GiornoInizio"]." ".$asta["OraInizio"].":00";
     $startDate = new DateTime($start);
     $end = $asta["AnnoFine"]."-".$asta["MeseFine"]."-".$asta["GiornoFine"]." ".$asta["OraFine"].":00";
     $endDate = new DateTime($end);
-
     if($date->getTimeStamp() < $startDate->getTimeStamp()){
         require($templateParams["auctionBefore"]);
     } else if($date->getTimeStamp() < $endDate->getTimeStamp()){
@@ -20,11 +21,12 @@
                 $asta["CodVincitore"]=$vincitore;
             }
         }
-        
-        if((isset($_SESSION["email"]) ? $asta["CodVincitore"] == $_SESSION["email"] : FALSE)){
-            require($templateParams["auctionWin"]);
-        } else {
-            require($templateParams["auctionLose"]);
+        if($endDate->getTimeStamp() > $oneWeekAgo->getTimeStamp()){
+            if((isset($_SESSION["email"]) ? $asta["CodVincitore"] == $_SESSION["email"] : FALSE)){
+                require($templateParams["auctionWin"]);
+            } else {
+                require($templateParams["auctionLose"]);
+            }
         }
     }
 }
