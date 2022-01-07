43eb62d546782f1417695e912b10d3469577b2a3
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2022-01-07 18:29:23 +0100

    Not completed (check css for rise atcion)

--- checkSeller.php
+++ checkSeller.php
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-    if(<?php echo !isset($_SESSION['email']) || $dbh->checkSeller()?>){
+    if(<?php echo (!isset($_SESSION['email']) || $dbh->checkSeller()) ? "true" : "false" ?>){
         if(!$("button#raise<?php echo $asta["IDProdotto"]?>").hasClass("unable")){
             $("button#raise<?php echo $asta["IDProdotto"]?>").addClass("unable");
         }
--- template/raise.php
+++ template/raise.php
@@ -3,9 +3,9 @@
             if (!event) event = window.event;
             event.stopPropagation();
             var actual = document.getElementById("price<?php echo $asta["IDProdotto"]?>").value;
-            $(this).prev().replaceWith("<form action='raise.php?id=<?php echo $asta["IdAsta"]?>&base=" + actual +"' method='POST' enctype='multipart/form-data'><select class='raise' name='raise' id='select<?php echo $asta["IDProdotto"]?>' onclick='selectClick(event)'><option value='5'>5</option><option value='10'>10</option><option value='50'>50</option><option value='100'>100</option></select><input type='submit' value='raise'/>");
+            $(this).prev().replaceWith("<form action='raise.php?id=<?php echo $asta["IdAsta"]?>&base=" + actual +"' method='POST' enctype='multipart/form-data'><select class='raise' name='raise' id='select<?php echo $asta["IDProdotto"]?>' onclick='selectClick(event)'><option value='5'>5</option><option value='10'>10</option><option value='50'>50</option><option value='100'>100</option></select><input class='last1' type='submit' value='raise'/></form>");
             $(this).next().replaceWith("<button class='last2 grey back' id='back<?php echo $asta["IDProdotto"]?>'>BACK</button></form>");
-            $(this).replaceWith("<input type='submit' class='last1 grey raise' id='raise<?php echo $asta["IDProdotto"]?>'/>");
+            $(this).replaceWith("");
 
             document.getElementById('back<?php echo $asta["IDProdotto"]?>').onclick = function(event) {
                 if (!event) event = window.event;
