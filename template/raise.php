a6bedca82ad54ba143e4e4752fe3b8e0f1d1b5ac
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2022-01-08 16:42:31 +0100

    Solved css for mobile, css for raise action

--- template/raise.php
+++ template/raise.php
@@ -3,15 +3,9 @@
             if (!event) event = window.event;
             event.stopPropagation();
             var actual = document.getElementById("price<?php echo $asta["IDProdotto"]?>").value;
-            $(this).prev().replaceWith("<form action='raise.php?id=<?php echo $asta["IdAsta"]?>&base=" + actual +"' method='POST' enctype='multipart/form-data'><select class='raise' name='raise' id='select<?php echo $asta["IDProdotto"]?>' onclick='selectClick(event)'><option value='5'>5</option><option value='10'>10</option><option value='50'>50</option><option value='100'>100</option></select><input class='last1' type='submit' value='raise'/></form>");
-            $(this).next().replaceWith("<button class='last2 grey back' id='back<?php echo $asta["IDProdotto"]?>'>BACK</button></form>");
+            $(this).prev().replaceWith("<form action='raise.php?id=<?php echo $asta["IdAsta"]?>&base=" + actual +"' method='POST' enctype='multipart/form-data'><select class='raise' name='raise' id='select<?php echo $asta["IDProdotto"]?>' onclick='selectClick(event)'><option value='5'>5</option><option value='10'>10</option><option value='50'>50</option><option value='100'>100</option></select><button class='last1 grey raise' type='submit'>RAISE</button><button class='last2 grey back' type='submit' formaction='back.php'>BACK</button></form>");
+            $(this).next().replaceWith("");
             $(this).replaceWith("");
-
-            document.getElementById('back<?php echo $asta["IDProdotto"]?>').onclick = function(event) {
-                if (!event) event = window.event;
-                event.stopPropagation();
-                document.location.reload(true);
-            }
         }
 
     </script>
