ef01d662d35dbe21f067810d5e116627e92af846
Emanuele Orlietti <emanuele.orlietti@studio.unibo.it>
2022-01-14 19:09:56 +0100

    Finished login and registration

--- template/login.php
+++ template/login.php
@@ -2,13 +2,13 @@
     <h1>LOGIN</h1>
     <ul>
         <li>
-            <label for="email">Email: </label><input type="text" id="email" name="email" /><br>
+            <label for="email">Email: </label><input type="text" id="email" name="email" placeholder="Email"/><br>
         </li>
     </ul>
     <ul>
         <li>
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password"/>
+            <input type="password" id="password" name="password" placeholder="Password"/>
             <img type="button" src="./img/eye.png" id="showHidePwd"/>
         </li>
     </ul>
