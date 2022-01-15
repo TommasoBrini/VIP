05531793a8732391c2bcc548c3e99d1d4abef624
Emanuele Orlietti <emanuele.orlietti@studio.unibo.it>
2022-01-15 16:39:11 +0100

    Update cart

--- template/cart.php
+++ template/cart.php
@@ -13,20 +13,22 @@
     </thead>
     <tbody>
     <?php foreach($templateParams["rows"] as $row){
-        $photo = getPhoto($row["CodProdotto"]);
-        $name = getName($row["CodProdotto"]);
-        $unitPrice = getUnitPrice($row["CodProdotto"]);
-        $quantity = $row["Quantità"];
+        $photo = $dbh->getPhoto($row["CodProdotto"]);
+        $name = $dbh->getName($row["CodProdotto"]);
+        $unitPrice = $dbh->getUnitPrice($row["CodProdotto"]);
+        $quantityAvailable = $dbh->getQuantityAvailable($row["CodProdotto"]);
+        $quantity = $dbh->getQuantity($row["CodOrdine"],$row["CodProdotto"]);
         $total = $quantity*$unitPrice;
+    }
     ?>
         <tr class="border_bottom">
             <td id="selected" class="cell_selected"><input type='checkbox' id='selected'></td>
-            <td id="photo" class="cell_photo"><img class="flex" src="<?php echo UPLOAD_DIR.$photo;?>"></td>
+            <td id="photo" class="cell_photo"><img class="flex" src="<?php echo UPLOAD_DIR.$photo;?>"/></td>
             <td id="name" class="cell_name"><?php echo $name?></td>
             <td id="unitPrice" class="cell_unitPrice"><?php echo $unitPrice?></td>
-            <td id="quantity" class="cell_quantity"><input type='number' id='quantity' name='quantity'></td>
-            <td id="total" class="cell_total">€</td>
-            <td id="trash"><img type="button" src="./img/trash.png" id="trash" onClick="deleteRow(<?php echo $row["IdRiga"]?>)"/><br>
+            <td id="quantity" class="cell_quantity"><input type='number' id='quantity' name='quantity' min=1 max='<?php $quantityAvailable ?>' value='<?php $quantity ?>'></td>
+            <td id="total" class="cell_total"><?php echo $total ?></td>
+            <td id="trash"><img type="button" src="./img/trash.png" id="trash" onClick="deleteRow(<?php echo $row["IdRiga"]?>)"><br>
         </tr>
     </tbody>
     <tfoot>
