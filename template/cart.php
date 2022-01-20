1e12b69d35a560b9f76047b9479882178278ac97
Emanuele Orlietti <emanuele.orlietti@studio.unibo.it>
2022-01-20 13:19:10 +0100

    Update Cart

--- template/cart.php
+++ template/cart.php
@@ -12,31 +12,24 @@
         </tr>
     </thead>
     <tbody>
-    <?php foreach($templateParams["rows"] as $row){
-        $photo = $dbh->getPhoto($row["CodProdotto"]);
-        $name = $dbh->getName($row["CodProdotto"]);
-        $unitPrice = $dbh->getUnitPrice($row["CodProdotto"]);
-        $quantityAvailable = $dbh->getQuantityAvailable($row["CodProdotto"]);
-        $quantity = $dbh->getQuantity($row["CodOrdine"],$row["CodProdotto"]);
-        $total = $quantity*$unitPrice;
-    }
-    ?>
+    <?php foreach($templateParams["rows"] as $row): ?>
         <tr class="border_bottom">
-            <td id="selected" class="cell_selected"><input type='checkbox' id='selected'></td>
-            <td id="photo" class="cell_photo"><img class="flex" src="<?php echo UPLOAD_DIR.$photo;?>"/></td>
-            <td id="name" class="cell_name"><?php echo $name?></td>
-            <td id="unitPrice" class="cell_unitPrice"><?php echo $unitPrice?></td>
-            <td id="quantity" class="cell_quantity"><input type='number' id='quantity' name='quantity' min=1 max='<?php $quantityAvailable ?>' value='<?php $quantity ?>'></td>
-            <td id="total" class="cell_total"><?php echo $total ?></td>
-            <td id="trash"><img type="button" src="./img/trash.png" id="trash" onClick="deleteRow(<?php echo $row["IdRiga"]?>)"><br>
+        <td id="selected" class="cell_selected"><input type='checkbox' id='selected'></td>
+        <td id="photo" class="cell_photo"><img class="flex" src="<?php echo UPLOAD_DIR.$row["Immagine"];?>"/></td>
+        <td id="name" class="cell_name"><?php echo $row["Nome"]?></td>
+        <td id="unitPrice" class="cell_unitPrice"><?php echo $row["Prezzo"]?></td>
+        <td id="quantity" class="cell_quantity"><input type='number' id='quantity' name='quantity' min=1 max='<?php echo $row["Disponibilita"]?>' value='<?php echo $row["Quantita"]?>'></td>
+        <td id="total" class="cell_total"><?php echo $row["Quantita"] * $row["Prezzo"]?></td>
+        <td id="trash"><img type="button" src="./img/trash.png" id="trash" onClick="deleteRow(<?php echo $row["IdRiga"]?>)"/><br>
         </tr>
+    <?php endforeach; ?>
     </tbody>
     <tfoot>
         <tr>
             <td colspan="2"><button name="checkOut" id="checkOut">CHECK OUT</button></td>
             <td colspan="2">"Just for you" shipping is free</td>
             <td>TOTAL</td>
-            <td colspan="2">300000€</td>
+            <td colspan="2">Funzione in js per calcolo totale, solo selezionate</td>
         </tr>
     </tfoot>
 </table>
