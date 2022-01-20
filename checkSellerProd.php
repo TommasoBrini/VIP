f91d0a2d6c122edb3a00dfdc4ca514a47c3cd009
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2022-01-20 17:23:33 +0100

    Update

--- checkSellerProd.php
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-    if(<?php echo (!isset($_SESSION['email']) || $dbh->checkSeller()) ? "true" : "false" ?>){
-        if(!$("button#addCart<?php echo $product["IDProdotto"]?>").hasClass("unable")){
-            $("button#addCart<?php echo $product["IDProdotto"]?>").addClass("unable");
-        }
-    }
-</script>
--- raise.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "bootstrap.php";
-
-$raise = $_POST["raise"];
-
-$dbh->raise($_GET['id'], $_GET['base']+$raise, $_SESSION['email']);
-
-header("location:index.php");
-?>
