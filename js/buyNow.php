f38ceb47aa044e9460a6486671f2ab17107edf1c
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2022-01-24 18:10:35 +0100

    Solved bug

--- js/buyNow.php
+++ js/buyNow.php
@@ -2,7 +2,7 @@
         document.getElementById('buyNow<?php echo $asta["IDProdotto"]?>').onclick = function(event) {
             if (!event) event = window.event;
             event.stopPropagation();
-            $(this).prev().prev().prev().replaceWith("<button id='<?php echo "price".$asta["IDProdotto"]?>' class='yellow price'><?php echo number_format($asta["Prezzo"]); ?> €</button>");
+            $(this).prev().prev().prev().replaceWith("<button id='<?php echo "p".$asta["IDProdotto"]?>' class='yellow price'><?php echo number_format($asta["Prezzo"]); ?> €</button>");
             $(this).prev().prev().replaceWith("<button class='yellow label'>SICURO?</button>");
             $(this).prev().replaceWith("<button class='last1 grey' id='yes<?php echo $asta["IDProdotto"]?>'>YES</button>");
             $(this).replaceWith("<button class='last2 grey' id='no<?php echo $asta["IDProdotto"]?>'>NO</button>");
