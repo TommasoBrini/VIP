ab1b6e599d9ca38a4f753daa1ba364242a493d9c
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2022-01-24 18:16:00 +0100

    Bug fix

--- ajax/processToCart.php
+++ ajax/processToCart.php
@@ -6,8 +6,13 @@ require "../bootstrap.php";
     $prodottiIn = $_POST['idProdottiIn'];
     $email = $_POST['email'];
     $emailSeller = $dbh->getSeller();
-    $somma = $dbh->checkOut($idOrdine, $prodottiOut, $prodottiIn);
-
-    $dbh->insertNotify($email, "The order has been successfully!", NULL, $idOrdine, $somma , NULL);
-    $dbh->insertNotify($emailSeller, "è stato eff un'ordine",NULL, $idOrdine, $somma, NULL);
+    if (count($prodottiIn) > 1){
+        $somma = $dbh->checkOut($idOrdine, $prodottiOut, $prodottiIn);
+        $dbh->insertNotify($email, "The order has been successfully!", NULL, $idOrdine, $somma , NULL);
+        $dbh->insertNotify($emailSeller, "è stato eff un'ordine",NULL, $idOrdine, $somma, NULL);
+        header("Location: ../index_pagamento.php");
+    } else {
+        header("Location: ../index_cart.php");
+    }
+    
 ?>
